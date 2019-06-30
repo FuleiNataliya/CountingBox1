189bc35987b42e6e838712f67baecf90ede172cb
Fulei Nataliya <fulei.nataliya@gmail.com>
2019-06-30 19:43:29 +0300

    added class

---
+++ src/main/java/Count.java
@@ -0,0 +1,30 @@
+package main.java;
+
+import java.util.*;
+
+public class Count {
+    public static void count(int total) {
+
+        List<Integer> bills = new ArrayList<>(Arrays.asList(500, 200, 100, 50, 20, 10, 50, 2, 1));
+
+        //List<Integer> count = new ArrayList<>(Collections.nCopies(bills.size(), 0));
+        Iterator<Integer> iterator = bills.iterator();
+        Integer num = iterator.next();
+        int countNum = total;
+
+        while (iterator.hasNext()) {
+            int meter = countNum / num;
+            if (countNum >= num) {
+                System.out.println(num + "-" + meter);
+                countNum %= num;
+            }
+            if (countNum < num) {
+                num = iterator.next();
+            }
+            if (countNum == 0) {
+                break;
+            }
+        }
+
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
 package main.java;
 
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
     public static void main(String[] args) {
 
+       /* System.out.print("Enter the amount of money");
+        Scanner sum = new Scanner(System.in);*/
+
+        int rand = ThreadLocalRandom.current().nextInt(24565, 35789);
+        System.out.println(rand);
+
+        long start = System.currentTimeMillis();
+        Count.count();
+        long end = System.currentTimeMillis();
+        System.out.println("Time" + (end - start));
 
     }
 }
