465d807a0812f3d5dba94235bd82a7f3cfbd6471
Fulei Nataliya <fulei.nataliya@gmail.com>
2019-06-30 20:02:09 +0300

    correct class

--- src/main/java/Count.java
+++ src/main/java/Count.java
@@ -6,8 +6,6 @@ public class Count {
     public static void count(int total) {
 
         List<Integer> bills = new ArrayList<>(Arrays.asList(500, 200, 100, 50, 20, 10, 50, 2, 1));
-
-        //List<Integer> count = new ArrayList<>(Collections.nCopies(bills.size(), 0));
         Iterator<Integer> iterator = bills.iterator();
         Integer num = iterator.next();
         int countNum = total;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-       /* System.out.print("Enter the amount of money");
-        Scanner sum = new Scanner(System.in);*/
-
         int rand = ThreadLocalRandom.current().nextInt(24565, 35789);
         System.out.println(rand);
 
         long start = System.currentTimeMillis();
-        Count.count();
+        Count.count(rand);
         long end = System.currentTimeMillis();
-        System.out.println("Time" + (end - start));
+
+        System.out.println("Time " + (end - start) / 1000f);
 
     }
 }
